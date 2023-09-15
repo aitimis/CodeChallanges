3d58f2c3bd850875fa6cab185460d7aa0109ae1f
Alex Timis <timis.alexioan@gmail.com>
2023-09-15 14:05:53 +0300

    Solution for coding challange added

---
+++ 9-15-23/app.js
@@ -0,0 +1,21 @@
+// Create a function which answers the question "Are you playing banjo?".
+// If your name starts with the letter "R" or lower case "r", you are playing banjo!
+// The function takes a name as its only argument, and returns one of the following strings:
+
+// name + " plays banjo" 
+// name + " does not play banjo"
+// Names given are always valid strings.
+
+
+// my solution
+function areYouPlayingBanjo(name) {
+    if (name.startsWith("R") || name.startsWith("r")) {
+      return name + " plays banjo";
+    } else return name + " does not play banjo";
+  }
+
+
+// top solution from the codewars community
+function areYouPlayingBanjo(name) {
+    return name + (name[0].toLowerCase() == 'r' ? ' plays' : ' does not play') + " banjo";
+  } 
